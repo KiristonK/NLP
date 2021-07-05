25d8ae274172cb561a9d600b835e4bc22523decf
Vadym Kostyrkin <kiristonk@gmail.com>
2021-07-05 19:10:10 +0200

    Done

--- data.py
+++ data.py
@@ -1,26 +1,35 @@
-import os
 import nltk
+import pickle
 import pandas as pd
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
 
 
 class DataWorker:
     stopwords = nltk.corpus.stopwords.words('english')
 
-    def __init__(self, filename):
-        self.data = pd.read_csv(filename, index_col=0)
-        self.data.head()
-        print('=> ', self.data.shape[0], 'rows and', self.data.shape[1], 'columns loaded\n')
-        print('=> Counts:', self.data['product'].value_counts())
-        self.data.dropna(axis=0, inplace=True)
-        self.tfidf = TfidfVectorizer(analyzer=self.text_clean)
-        self.x_tfidf = self.tfidf.fit_transform(self.data['narrative'])
-        print('=> Matrix shape:', self.x_tfidf.shape)
+    def __init__(self, filename=""):
+        self.count_vect = CountVectorizer(analyzer=self.text_clean)
+        self.classifier = MultinomialNB()
+        self.tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2',
+                                     ngram_range=(1, 2), stop_words='english', analyzer=self.text_clean)
+        self.tfidf_transformer = TfidfTransformer()
+        if filename != "":
+            self.data = pd.read_csv(filename, index_col=0)
+            self.data.head()
+            self.data = self.data[['product', 'narrative']].sample(n=70000)
+            print('=> ', self.data.shape[0], 'rows and', self.data.shape[1], 'columns loaded\n')
+            print('=> Counts:\n', self.data['product'].value_counts())
+            self.data.dropna(axis=0, inplace=True)
+            self.features = self.tfidf.fit_transform(self.data['narrative'])
+            print('=> Matrix shape:', self.features.shape)
 
     def get_words_bag(self):
         feature_names = self.tfidf.get_feature_names()
-        return pd.DataFrame(self.x_tfidf.toarray(), columns=feature_names)
+        return pd.DataFrame(self.features.toarray(), columns=feature_names)
 
     # Returning input text as list of fords in lowercase without digits, words with length < 3, stopwords
     def text_clean(self, text):
@@ -36,3 +45,35 @@ class DataWorker:
                     else:
                         continue
         return clean_words
+
+    def create_predictor(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.data['narrative'], self.data['product'],
+                                                            random_state=0)
+        X_train_counts = self.count_vect.fit_transform(X_train)
+        tfidf_transformer = TfidfTransformer()
+        x_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+        self.classifier = MultinomialNB().fit(x_train_tfidf, y_train)
+
+    def save_predictor(self):
+        with open('count_vect.pickle', 'wb') as picklefile:
+            pickle.dump(self.count_vect, picklefile)
+
+        with open('text_classifier.pickle', 'wb') as picklefile:
+            pickle.dump(self.classifier, picklefile)
+
+    def load_predictor(self):
+        with open('count_vect.pickle', 'rb') as vectorizer:
+            self.count_vect = pickle.load(vectorizer)
+
+        with open('text_classifier.pickle', 'rb') as training_model:
+            self.classifier = pickle.load(training_model)
+
+    def predict(self, data):
+        prediction = self.classifier.predict(data)
+
+        print(prediction)
+        return prediction
+
+    def predict_string(self, text):
+        prediction = self.classifier.predict(self.count_vect.transform([text]))
+        return prediction
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-import os
-import nltk
-import numpy as np
-import pandas as pd
-import pandas as py
+
 
 from data import DataWorker
 
-dw = DataWorker('complaints_processed.csv')
+# dla trenowania modelu
+#dw = DataWorker('complaints_processed.csv')
+#dw.create_predictor()
 
-print(dw.get_words_bag())
+dw = DataWorker()
+dw.load_predictor()
+
+
+dw.save_predictor()
+
+print('\n\nTest input: shit payment')
+pr = dw.predict_string('shit payment')
+print('Text classified as: ' + pr[0])
+
+print('Test input: nice place')
+pr = dw.predict_string('nice place')
+print('Text classified as: ' + pr[0])
+
+print('Test input: hate employees, they are unpleasant')
+pr = dw.predict_string('hate employees, they are unpleasant')
+print('Text classified as: ' + pr[0])
+
+print('Test input: within day closing told using lender would cause purchase agreement price go even appraisal done also give option purchasing loan without pmi said absolutely possible loan le include pmi forcing choose product want signed contract sale consultant told use lender losing incentive closing cost')
+pr = dw.predict_string('within day closing told using lender would cause purchase agreement price go even appraisal done also give option purchasing loan without pmi said absolutely possible loan le include pmi forcing choose product want signed contract sale consultant told use lender losing incentive closing cost')
+print('Text classified as: ' + pr[0])
+
+print('Test input: creditor refuse provide income statement account receivable account payable')
+pr = dw.predict_string('creditor refuse provide income statement account receivable account payable')
+print('Text classified as: ' + pr[0])
